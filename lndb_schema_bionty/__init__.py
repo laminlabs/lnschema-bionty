9762f255f0dfdc03074768a69bd1216241c58022
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-31 11:38:50 +0200

    :green_heart: Actually add

---
+++ lndb_schema_bionty/__init__.py
@@ -0,0 +1,12 @@
+"""Default schema module biology."""
+
+__version__ = "0.1.2"
+
+from ._core import (  # noqa
+    featureset,
+    featureset_gene,
+    featureset_protein,
+    gene,
+    protein,
+    species,
+)
