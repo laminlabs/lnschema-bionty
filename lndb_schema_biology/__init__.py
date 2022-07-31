87b79a42c905dbd57bba902ca90adfd45d2e46b7
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-31 11:35:22 +0200

    :truck: Move biolab to wetlab schema module

--- lndb_schema_biology/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Default schema module biology."""
-
-__version__ = "0.1.2"
-
-from . import biolab, bionty
--- lndb_schema_biology/biolab.py
+++
@@ -1,1 +0,0 @@
-from ._biolab import biometa, biosample, dobject_biometa, readout_type  # noqa
