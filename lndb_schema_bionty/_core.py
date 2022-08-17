5f1464a38fa624e06c75b2bd620eea36f357fb7a
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-17 15:02:05 +0200

    :bento: Updated the gene table to match new ensembl table

--- lndb_schema_bionty/_core.py
+++ lndb_schema_bionty/_core.py
@@ -45,21 +45,15 @@ class gene(SQLModel, table=True):  # type: ignore
     """Gene table."""
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    hgnc_symbol: str = Field(default=None)
-    mgi_symbol: str = Field(default=None)
-    species: int = Field(default=None, foreign_key="species.id")
-    name: str = Field(default=None)
-    hgnc_id: str = Field(default=None)
-    entrez_gene_id: int = Field(default=None)
     ensembl_gene_id: str = Field(default=None)
-    alias_symbol: str = Field(default=None)
-    locus_group: str = Field(default=None)  # sqlmodel doeesn't work with Literal yet
-    locus_type: str = Field(default=None)
-    location: str = Field(default=None)
-    vega_id: str = Field(default=None)
-    ucsc_id: str = Field(default=None)
-    rgd_id: str = Field(default=None)
+    name: str = Field(default=None)
+    synonym: str = Field(default=None)
+    species: int = Field(default=None, foreign_key="species.id")
+    gene_type: str = Field(default=None)
+    entrez_gene_id: int = Field(default=None)
+    hgnc_id: str = Field(default=None)
     omim_id: str = Field(default=None)
+    mgi_id: str = Field(default=None)
 
 
 class protein(SQLModel, table=True):  # type: ignore
