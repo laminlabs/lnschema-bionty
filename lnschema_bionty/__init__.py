b7ba9fbaea1f3cdba830919b908c4e18d123b5f0
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-19 12:03:01 +0200

    :green_heart: Clear cache

---
+++ lnschema_bionty/__init__.py
@@ -0,0 +1,33 @@
+"""Schema module for universal biological entities (`zdno`).
+
+Import the package::
+
+   import lnschema_bionty
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   gene
+   protein
+   species
+   featureset
+   featureset_gene
+   featureset_protein
+   version_zdno
+
+"""
+# This is lnschema-module zdno.
+_schema_module_id = "zdno"
+__version__ = "0.1.4"
+
+from ._core import (  # noqa
+    featureset,
+    featureset_gene,
+    featureset_protein,
+    gene,
+    protein,
+    species,
+    version_zdno,
+)
