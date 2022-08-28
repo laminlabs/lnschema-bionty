da2628580854fe52ed0f823a0bd9db1ac5fae705
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-08-28 22:45:42 +0200

    :art: Import additional modules, species -> species_id

--- lnschema_bionty/__init__.py
+++ lnschema_bionty/__init__.py
@@ -11,10 +11,15 @@ This is the complete API reference:
 
    gene
    protein
+   cell_marker
+   cell_type
    species
+   tissue
+   disease
    featureset
    featureset_gene
    featureset_protein
+   featureset_cell_marker
    version_zdno
 
 """
@@ -23,11 +28,16 @@ _schema_module_id = "zdno"
 __version__ = "0.2.2"
 
 from ._core import (  # noqa
+    cell_marker,
+    cell_type,
+    disease,
     featureset,
+    featureset_cell_marker,
     featureset_gene,
     featureset_protein,
     gene,
     protein,
     species,
+    tissue,
     version_zdno,
 )
