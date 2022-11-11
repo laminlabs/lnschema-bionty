6c6dcc85d2555d49accabbd03e480e665d19d59b
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-11-12 01:46:36 +0800

    :zap: Implemented knowledge as a decorator

--- lnschema_bionty/dev/_bionty.py
+++ lnschema_bionty/dev/_bionty.py
@@ -1,34 +1,50 @@
-from typing import Union
-
-from bionty import Gene, Protein, Species
+from bionty import EntityTable
 
 
 def fields_from_knowledge(
     locals: dict,
-    knowledge_table: Union[Species, Gene, Protein],
+    knowledge_table: EntityTable,
     default_factory_col="id",
 ):
     all_locals = locals.copy()
-    if default_factory_col is not None:
-        if all_locals.get(default_factory_col) is None:
-            locals.pop(default_factory_col)
+    if (default_factory_col is not None) and (default_factory_col in locals):
+        locals.pop(default_factory_col)
     kwargs = {}
     for k, v in all_locals.items():
-        if v is None or k in {"self", "__class__"}:
+        df = knowledge_table(id=k).df
+        if v not in df.index:
             continue
-        kwargs = knowledge_table(id=k).df.loc[v]
+        kwargs = df.loc[v]
     init_kwargs = locals
     pydantic_attrs = kwargs
     return init_kwargs, pydantic_attrs
 
 
-def init_sqlmodel_parent(parent, child, init_kwargs, pydantic_attrs):
-    parent.__init__(**init_kwargs)
-
+def init_sqlmodel_parent(model, pydantic_attrs):
     if len(pydantic_attrs) == 0:
         return
 
     for k, v in pydantic_attrs.items():
-        if k not in child.__fields__:
+        if k not in model.__fields__:
             continue
-        parent.__setattr__(k, v)
+        model.__setattr__(k, v)
+
+
+def knowledge(knowledge_table: EntityTable):
+    def test(original_class):
+        orig_init = original_class.__init__
+
+        def __init__(self, **kwargs):
+            init_kwargs, pydantic_attrs = fields_from_knowledge(
+                locals=kwargs, knowledge_table=knowledge_table
+            )
+
+            orig_init(self, **kwargs)
+
+            init_sqlmodel_parent(self, pydantic_attrs)
+
+        original_class.__init__ = __init__
+
+        return original_class
+
+    return test
