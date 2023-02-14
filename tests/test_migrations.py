abfa9672dc1417559f580837c2b909f392a5119d
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-02-14 22:08:39 +0800

    :fire: Remove migration files

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+from lndb._migrations import generate_module_files
 from lndb.test import (
     get_package_name,
     migrate_clones,
@@ -5,7 +8,10 @@ from lndb.test import (
     model_definitions_match_ddl,
 )
 
+from lnschema_bionty import _schema_id as schema_id
+
 package_name = get_package_name()
+migrations_path = Path(__file__).parent.parent / package_name / "migrations"
 
 
 def test_migration_id_is_consistent():
@@ -13,6 +19,7 @@ def test_migration_id_is_consistent():
 
 
 def test_model_definitions_match_ddl_postgres():
+    generate_module_files(package_name=package_name, migrations_path=migrations_path, schema_id=schema_id)
     model_definitions_match_ddl(package_name, dialect_name="postgresql")
 
 
@@ -22,7 +29,7 @@ def test_migrate_clones_sqlite():
         raise RuntimeError("Migration e2e test failed.")
 
 
-# def test_migrate_clones_postgres():
-#     results = migrate_clones(package_name, n_instances=1, dialect_name="postgresql")
-#     if "migrate-failed" in results:
-#          raise RuntimeError("Migration e2e test failed.")
+def test_migrate_clones_postgres():
+    results = migrate_clones(package_name, n_instances=1, dialect_name="postgresql")
+    if "migrate-failed" in results:
+        raise RuntimeError("Migration e2e test failed.")
