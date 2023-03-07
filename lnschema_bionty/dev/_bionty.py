c59faad8427bf12465c938fa1bcdda3e5a406bd9
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-03-07 10:28:07 +0100

    ✨ Use ontology_id as the id for Tissue, CellType, Disease (#57)
    * :sparkles: Use ontology_id as the id for Tissue, CellType, Disease
    
    * :arrow_up: updated dep

--- lnschema_bionty/dev/_bionty.py
+++ lnschema_bionty/dev/_bionty.py
@@ -4,17 +4,15 @@ from bionty import Entity
 def fields_from_knowledge(
     locals: dict,
     knowledge_table: Entity,
-    default_factory_col="id",
 ):
-    all_locals = locals.copy()
-    if (default_factory_col is not None) and (default_factory_col in locals):
-        locals.pop(default_factory_col)
     kwargs = {}
-    for k, v in all_locals.items():
+    for k, v in locals.items():
         df = knowledge_table(id=k).df
         if v not in df.index:
             continue
         kwargs = df.loc[v].to_dict()
+        if "ontology_id" in kwargs:
+            kwargs["id"] = kwargs["ontology_id"]
     pydantic_attrs = kwargs
     return pydantic_attrs
 
