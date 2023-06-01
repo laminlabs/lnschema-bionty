c50dda2a666d06f8715e948393cf469cc3309072
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-01 13:43:21 +0200

    :green_heart: Try fix

--- noxfile.py
+++ noxfile.py
@@ -5,13 +5,13 @@ from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 nox.options.default_venv_backend = "none"
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+@nox.session
 def lint(session: nox.Session) -> None:
     run_pre_commit(session)
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
-def build(session):
+@nox.session
+def build(session: nox.Session):
     session.run(*"pip install .[dev,test]".split())
     session.run(*"pip install git+https://github.com/laminlabs/lamindb".split())
     login_testuser1(session)
