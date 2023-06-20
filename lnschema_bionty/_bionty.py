776391125bd46faa4aabdc153c92b2fdc3a4ab5e
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-20 16:00:51 +0200

    🎨 Create multiple records with .from_bionty

--- lnschema_bionty/_bionty.py
+++ lnschema_bionty/_bionty.py
@@ -1,7 +1,8 @@
-from typing import Iterable, Optional, Union
+from typing import Iterable, List, Optional, Set, Union
 
 import bionty as bt
 from django.core.exceptions import ObjectDoesNotExist
+from lamin_logger import logger
 from lnschema_core.models import BaseORM
 
 from . import ids
@@ -10,37 +11,36 @@ from . import ids
 def fields_from_knowledge(
     locals: dict,
     bionty_object: bt.Bionty,
-):
-    df = bionty_object.df().reset_index()
-    bionty_dict = {}
+) -> List:
     if len(locals) > 1:
         raise AssertionError("Only 1 kwarg can be passed when populating fields from bionty!")
-    elif len(locals) == 1:
-        k = next(iter(locals))
-        v = locals[k]
-        try:
-            bionty_dict = df.set_index(k).loc[v].to_dict()
-        except KeyError:
-            raise ValueError(
-                f"No entry is found in bionty reference table with '{k}={v}'!\n Try passing a species other than {bionty_object.species}!"
-            )
-        bionty_dict[k] = v
+    df = bionty_object._df
 
-    return bionty_dict
+    k = next(iter(locals))
+    v = locals[k]
+    try:
+        bionty_dicts = df.set_index(k).loc[[v]].reset_index().to_dict(orient="records")
+    except KeyError:
+        raise ValueError(f"No entry is found in bionty reference table with '{k}={v}'!\n Try passing a species other than {bionty_object.species}!")
+
+    return bionty_dicts
 
 
 def create_or_get_species_record(species: Union[str, BaseORM]) -> Optional[BaseORM]:
     if isinstance(species, BaseORM):
         species_record = species
-    elif isinstance(species, str) and species != "all":
+    elif isinstance(species, str):
         from lnschema_bionty import Species
 
         try:
             species_record = Species.objects.get(name=species)
         except ObjectDoesNotExist:
-            species_record = Species.from_bionty(name=species)
-            species_record.bionty_source = get_bionty_source_record(bt.Species())
-            species_record.save()
+            try:
+                species_record = Species.from_bionty(name=species)
+                species_record.bionty_source = get_bionty_source_record(bt.Species())
+                species_record.save()
+            except KeyError:
+                species_record = None
     else:
         species_record = None
 
@@ -72,19 +72,40 @@ def get_bionty_object(model: BaseORM, species: Optional[str] = None):
         return bionty_object
 
 
-def _add_synonym(synonym: Union[str, Iterable], record: BaseORM):
+def _add_synonym(synonym: Union[str, Iterable], record: BaseORM, force: bool = False):
     """Append new synonym to a synonym field."""
-    # nothing happens when passing an empty string
     if isinstance(synonym, str):
-        if len(synonym) == 0:
-            return
-        synonym = [synonym]
+        synonym = set([synonym])
     else:
-        synonym = list(synonym)
-
+        synonym = set(synonym)
+    # nothing happens when passing an empty string or list
+    if len(synonym) == 0:
+        return
     # because we use | as the separator
     if any(["|" in i for i in synonym]):
         raise AssertionError("A synonym can't contain '|'!")
+
+    def check_synonyms_in_all_records(synonyms: Set[str], record: BaseORM):
+        """Errors if the input synonyms are already associated with records in the DB."""
+        import pandas as pd
+        from IPython.display import display
+
+        syns_all = record.__class__.objects.exclude(synonyms="").exclude(synonyms=None).all()
+        df = pd.DataFrame(syns_all.values())
+        df["synonyms"] = df["synonyms"].str.split("|")
+        df = df.explode("synonyms")
+        matches_df = df[df["synonyms"].isin(synonyms)]
+        if matches_df.shape[0] > 0:
+            records_df = pd.DataFrame(syns_all.filter(id__in=matches_df["id"]).values())
+            logger.error(
+                f"Input synonyms {matches_df['synonyms'].unique()} already associated"
+                " with the following records:\n(Pass `force=True` to ignore this error)"
+            )
+            display(records_df)
+            raise SystemExit(AssertionError)
+
+    if not force:
+        check_synonyms_in_all_records(synonym, record)
 
     # only add if the passed synonym aren't already in synonyms
     synonyms = record.synonyms
@@ -123,26 +144,34 @@ def bionty_decorator(django_class):
         species_name = species.name if species is not None else None
         bionty_object = django_class.bionty(species=species_name)
 
-        if isinstance(lookup_result, tuple) and lookup_result is not None:
-            lookup_dict = lookup_result._asdict()  # type:ignore
-            bionty_dict = _encode_id(lookup_dict)
-        else:
-            bionty_dict = fields_from_knowledge(locals=kwargs, bionty_object=bionty_object)
-            bionty_dict = _encode_id(bionty_dict)
-
-        if getattr(django_class, "species", False):
-            if species is None:
-                raise AssertionError("Must specify a species!")
+        if lookup_result is not None:
+            if isinstance(lookup_result, list):
+                lookups = lookup_result
             else:
-                bionty_dict["species"] = species
-        if getattr(django_class, "bionty_source", False):
-            bionty_dict["bionty_source"] = get_bionty_source_record(bionty_object)
+                lookups = [lookup_result]
 
-        # filter for model fields
-        fields = [i.name for i in django_class._meta.fields]
-        bionty_dict_fields = {k: v for k, v in bionty_dict.items() if k in fields}
+            bionty_dicts = [_encode_id(i._asdict()) for i in lookups]  # type:ignore
+        else:
+            bionty_dicts = fields_from_knowledge(locals=kwargs, bionty_object=bionty_object)
+            bionty_dicts = [_encode_id(bd) for bd in bionty_dicts]
 
-        return django_class(**bionty_dict_fields)
+        records = []
+        for bionty_dict in bionty_dicts:
+            if getattr(django_class, "species", False):
+                if species is None:
+                    raise AssertionError("Must specify a species!")
+                else:
+                    bionty_dict["species"] = species
+            if getattr(django_class, "bionty_source", False):
+                bionty_dict["bionty_source"] = get_bionty_source_record(bionty_object)
+
+            # filter for model fields
+            fields = [i.name for i in django_class._meta.fields]
+            bionty_dict_fields = {k: v for k, v in bionty_dict.items() if k in fields}
+
+            records.append(django_class(**bionty_dict_fields))
+
+        return records if len(records) > 1 else records[0]
 
     def _encode_id(kwargs: dict):
         name = django_class.__name__.lower()
@@ -177,8 +206,12 @@ def bionty_decorator(django_class):
             kwargs["id"] = id_encoder(concat_str)
         return kwargs
 
-    def add_synonym(self, synonym: Union[str, Iterable]):
-        _add_synonym(synonym=synonym, record=self)
+    def add_synonym(self, synonym: Union[str, Iterable], force: bool = False):
+        try:
+            self.__getattribute__("synonyms")
+        except AttributeError:
+            raise NotImplementedError(f"No synonyms field found in table {self.__class__.__name__}!")
+        _add_synonym(synonym=synonym, record=self, force=force)
 
     django_class.from_bionty = from_bionty
     django_class.bionty = bionty
