c1c6e3e59347e66ecb7e014f2e25af6b98996c50
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-06-29 14:25:05 +0200

    🔖 Release 0.25.1

--- lnschema_bionty/__init__.py
+++ lnschema_bionty/__init__.py
@@ -32,7 +32,7 @@ Bionty sources:
 # This is lnschema-module zdno.
 _schema_id = "zdno"
 _name = "bionty"
-__version__ = "0.25.0"  # Denote a release candidate of version 0.1.0 with 0.1rc1
+__version__ = "0.25.1"  # Denote a release candidate of version 0.1.0 with 0.1rc1
 
 
 from lamindb_setup._check_instance_setup import (
