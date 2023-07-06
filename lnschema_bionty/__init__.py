f9614daa7efeba7a85a336ba9ca8490530413318
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-06 17:33:33 +0200

    🚸 Trigger instance loading for users

--- lnschema_bionty/__init__.py
+++ lnschema_bionty/__init__.py
@@ -42,19 +42,23 @@ Developer API:
 
    dev
 """
-# This is lnschema-module zdno.
 _schema_id = "zdno"
 _name = "bionty"
 __version__ = "0.26.1"  # Denote a release candidate of version 0.1.0 with 0.1rc1
 
+from lamindb_setup import _check_instance_setup
 
-from lamindb_setup._check_instance_setup import (
-    check_instance_setup as _check_instance_setup,
-)
 
-_INSTANCE_SETUP = _check_instance_setup()
+# trigger instance loading if users
+# want to access attributes
+def __getattr__(name):
+    if name not in {"models"}:
+        _check_instance_setup(from_lamindb=True)
+    return globals()[name]
 
-if _INSTANCE_SETUP:
+
+if _check_instance_setup():
+    del __getattr__  # delete so that imports work out
     from .dev._settings import settings
     from .models import (  # noqa
         BiontySource,
