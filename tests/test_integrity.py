6bde0ff05de5399126da21b704b12b6c16e68478
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-18 09:19:03 +0200

    ✅ Add integrity test (#151)
    * ✅ Add integrity test
    
    * 💚 Load bionty module

---
+++ tests/test_integrity.py
@@ -0,0 +1,13 @@
+import lamindb_setup as ln_setup
+import pytest
+
+
+@pytest.fixture
+def setup_instance():
+    ln_setup.init(storage="./testdb", schema="bionty")
+    yield
+    ln_setup.delete("testdb")
+
+
+def test_migrate_check(setup_instance):
+    assert ln_setup.migrate.check()
