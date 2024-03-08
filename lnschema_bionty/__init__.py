a13b12821277d9607d99692ab8f727cf0f75684f
Alex Wolf <f.alexander.wolf@gmail.com>
2024-03-08 08:04:17 +0100

    Remove auto-triggering load

--- lnschema_bionty/__init__.py
+++ lnschema_bionty/__init__.py
@@ -7,17 +7,7 @@ Since version 0.40.0, `bionty` replaces `lnschema-bionty` as the user-facing pac
 __version__ = "0.41.1"  # Denote a release candidate of version 0.1.0 with 0.1rc1
 
 from lamindb_setup import _check_instance_setup
-
 from . import ids
-
-
-# trigger instance loading if users
-# want to access attributes
-def __getattr__(name):
-    if name not in {"models"}:
-        _check_instance_setup(from_lamindb=True)
-    return globals()[name]
-
 
 if _check_instance_setup():
     del __getattr__  # delete so that imports work out
