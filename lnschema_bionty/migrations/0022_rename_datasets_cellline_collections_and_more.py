1d35ecb264d564db78dc971e2935947303f02cc8
Alex Wolf <f.alexander.wolf@gmail.com>
2024-05-16 17:33:40 +0200

    💚 Fix

--- lnschema_bionty/migrations/0022_rename_datasets_cellline_collections_and_more.py
+++ lnschema_bionty/migrations/0022_rename_datasets_cellline_collections_and_more.py
@@ -12,7 +12,7 @@ import lnschema_bionty.ids
 class Migration(migrations.Migration):
     initial = True
     dependencies = [
-        ("lnschema_core", "0001_initial_squashed_0037"),
+        ("lnschema_core", "0037_rename_dataset_to_collection"),
     ]
 
     operations = [
